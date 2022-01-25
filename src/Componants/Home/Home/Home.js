0f4aae6757592a401473e5b2b4c9bc1c3c1fcf6e
Farhan Elias <faarhaan10@gmail.com>
2022-01-26 04:17:26 +0600

    footer added

--- src/Componants/Home/Home/Home.js
+++ src/Componants/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
 import Banner from '../Banner/Banner';
 import OurBlogs from '../Blogs/OurBlogs/OurBlogs';
@@ -9,6 +10,7 @@ const Home = () => {
             <Navigation />
             <Banner />
             <OurBlogs />
+            <Footer />
         </div>
     );
 };
