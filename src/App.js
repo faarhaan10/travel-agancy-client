e49f528b7b242f97505db934b48191bc3a1aeba0
Farhan Elias <faarhaan10@gmail.com>
2022-01-26 15:56:10 +0600

    not-found asset

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import Home from './Componants/Home/Home/Home';
 import Login from './Componants/Authentication/Login/Login';
 import Registration from './Componants/Authentication/Registration/Registration';
+import NotFound from './Componants/NotFound/NotFound';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
