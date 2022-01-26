bae841b535f23055f41b29460f6c08b2b4a0f908
Farhan Elias <faarhaan10@gmail.com>
2022-01-26 18:13:28 +0600

    firebase and context

--- src/App.js
+++ src/App.js
@@ -8,18 +8,21 @@ import Home from './Componants/Home/Home/Home';
 import Login from './Componants/Authentication/Login/Login';
 import Registration from './Componants/Authentication/Registration/Registration';
 import NotFound from './Componants/NotFound/NotFound';
+import AuthProvider from "./context/AuthProvider/AuthProvider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="*" exact element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="*" exact element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,13 +1,137 @@
-import React from 'react';
-import firebaseAuthentication from '../Firebase/firebase.init';
+import axios from "axios";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useEffect, useState } from "react";
+import firebaseAuthentication from "../Firebase/firebase.init";
+
 firebaseAuthentication();
+
+
 const useFirebase = () => {
+    const [user, setUser] = useState({});
+    const [error, setError] = useState('');
+    const [admin, setAdmin] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
-    return (
-        <div>
+    //my database url
+    const databaseUrl = 'https://rizas-parlour.herokuapp.com';
 
-        </div>
-    );
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+
+    const handleCreateUser = (name, email, password, navigate) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const newUser = { email, displayName: name };
+                // saveUser(newUser, 'post');
+                handleUpdateUser(name);
+                navigate('/');
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
+    };
+
+    const handleUpdateUser = name => {
+        updateProfile(auth.currentUser, {
+            displayName: name
+        }).then(() => {
+            // Profile updated!
+            // ...
+        }).catch((error) => {
+            setError(error.message);
+        });
+    }
+
+    const handleUserLogin = (email, password, navigate, location) => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const destination = location?.state?.from || '/';
+                navigate(destination);
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
+    }
+
+
+    const handleGoogleLogin = (navigate, location) => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const user = result.user;
+                const newUser = {
+                    email: user.email,
+                    displayName: user.displayName
+                }
+                // saveUser(newUser, 'put');
+                const destination = location?.state?.from || '/';
+                navigate(destination);
+            })
+            .catch(error => {
+                setError(error.message);
+            });
+    };
+
+    const handleLogOut = () => {
+        setIsLoading(true);
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            setUser({});
+        }).catch((error) => {
+            // An error happened.
+            setUser({});
+            setError(error.message);
+        });
+    };
+
+    // manage user
+    useEffect(() => {
+        setIsLoading(true);
+        const unsubscribed = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                // User is signed out
+                setUser({});
+            }
+            setIsLoading(false);
+        });
+        return () => unsubscribed;
+    }, [auth])
+
+
+    //save user to db
+    // const saveUser = (newUser, method) => {
+    //     if (method === 'put') {
+    //         axios.put(`${databaseUrl}/users`, newUser)
+    //             .then()
+    //     }
+    //     else {
+    //         axios.post(`${databaseUrl}/users`, newUser)
+    //             .then()
+    //     }
+
+    // };
+
+    // // check admin
+    // useEffect(() => {
+    //     axios.get(`${databaseUrl}/users?email=${user.email}`)
+    //         .then(res => setAdmin(res.data.admin))
+    //         .catch()
+    // }, [user.email]);
+
+    return {
+        user,
+        handleLogOut,
+        handleGoogleLogin,
+        handleCreateUser,
+        handleUserLogin,
+        error,
+        databaseUrl,
+        admin,
+        isLoading
+    }
 };
 
 export default useFirebase;
